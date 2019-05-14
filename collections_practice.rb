e8eaa4a863bd3cee70a4abbba205c02067e6533f
Dan Kagan <dankagan7@gmail.com>
2019-05-14 01:04:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,7 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort do {|a, b| if (a > b) : b <=> a}
+  array.sort do {|a, b| if (a > b) b <=> a}
     binding.pry
   end
 end
